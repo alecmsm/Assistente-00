954fb685a08d0daf4e8f90d31ad597b34dcfabea
Alec <alecmsm@gmail.com>
2026-05-04 16:57:11 -0300

    Config correta

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Ele está protegido pelo .gitignore
 
 const CONFIG = {
-  CLIENT_ID: 'SEU_CLIENT_ID_AQUI'
+  CLIENT_ID: '12220583127-1m8his4qepd6umd6qkjgnb5gcdbu8os2.apps.googleusercontent.com'
 };
