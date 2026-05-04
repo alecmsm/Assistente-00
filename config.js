365f5fff058f9f110f50d3ee8cfb4dc33d9e3c3a
alecmsm <alecmsm@gmail.com>
2026-05-04 16:17:49 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Arquivo de configuração público - NÃO coloque seu Client ID aqui
+// O arquivo config.js real fica apenas no seu computador local
+
+const CONFIG = {
+  CLIENT_ID: 'SEU_CLIENT_ID_AQUI'
+};
