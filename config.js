4527b4b11073c37019f0ca59cef798b1e89e4edd
Alec <alecmsm@gmail.com>
2026-05-04 16:57:55 -0300

    Config correta com client id

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-// ⚠️ ESTE ARQUIVO NUNCA DEVE SER ENVIADO AO GITHUB
-// Ele está protegido pelo .gitignore
-
-const CONFIG = {
-  CLIENT_ID: '12220583127-1m8his4qepd6umd6qkjgnb5gcdbu8os2.apps.googleusercontent.com'
-};
+const CONFIG = { CLIENT_ID: '12220583127-1m8his4qepd6umd6qkjgnb5gcdbu8os2.apps.googleusercontent.com' };
