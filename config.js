77a983d1f388e3d33511f294729df4de4cae2448
Alec <alecmsm@gmail.com>
2026-05-04 17:00:16 -0300

    Config formatada corretamente

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-const CONFIG = { CLIENT_ID: '12220583127-1m8his4qepd6umd6qkjgnb5gcdbu8os2.apps.googleusercontent.com' };
+const CONFIG = {
+  CLIENT_ID: "12220583127-1m8his4qepd6umd6qkjgnb5gcdbu8os2.apps.googleusercontent.com"
+};
